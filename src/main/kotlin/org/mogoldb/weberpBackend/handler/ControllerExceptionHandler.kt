7d5d00fbe4e3017db99351ef3681bf2deaee4687
Gabriel Pagotto <gabriel.pagotto@icloud.com>
2023-01-06 19:51:00 -0300

    feat: add handleValidationExceptions handler

--- src/main/kotlin/org/mogoldb/weberpBackend/handler/ControllerExceptionHandler.kt
+++ src/main/kotlin/org/mogoldb/weberpBackend/handler/ControllerExceptionHandler.kt
@@ -9,8 +9,13 @@ import org.mogoldb.weberpBackend.payload.response.DuplicatedEntryErrorResponse
 import org.mogoldb.weberpBackend.payload.response.ErrorResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.validation.FieldError
+import org.springframework.validation.ObjectError
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import java.util.function.Consumer
 
 @ControllerAdvice
 class ControllerExceptionHandler {
@@ -52,5 +57,17 @@ class ControllerExceptionHandler {
             ex.field,
             "The value of this field cannot be duplicated in the database")
         return ResponseEntity.status(status).body(errorMessage)
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleValidationExceptions(ex: MethodArgumentNotValidException): Map<String, String?>? {
+        val errors: MutableMap<String, String?> = HashMap()
+        ex.bindingResult.allErrors.forEach(Consumer { error: ObjectError ->
+            val fieldName = (error as FieldError).field
+            val errorMessage = error.getDefaultMessage()
+            errors[fieldName] = errorMessage
+        })
+        return errors
     }
 }
