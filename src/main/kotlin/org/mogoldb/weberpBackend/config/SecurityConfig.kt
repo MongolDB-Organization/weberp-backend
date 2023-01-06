f4277ed87a4cd07b4d1358a965a9454bc95eb10a
Gabriel Pagotto <gabriel.pagotto@icloud.com>
2023-01-06 20:23:46 -0300

    update: change imports

--- src/main/kotlin/org/mogoldb/weberpBackend/config/SecurityConfig.kt
+++ src/main/kotlin/org/mogoldb/weberpBackend/config/SecurityConfig.kt
@@ -11,9 +11,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.web.SecurityFilterChain
