65de5e59af8ef791393a6c0327c18e8d121d9070
Gabriel Pagotto <gabriel.pagotto@icloud.com>
2023-01-10 20:16:20 -0300

    fix: email validation

--- src/main/kotlin/org/mogoldb/weberpBackend/controller/v1/autenticacao/payload/request/CadastroRequest.kt
+++ src/main/kotlin/org/mogoldb/weberpBackend/controller/v1/autenticacao/payload/request/CadastroRequest.kt
@@ -1,11 +1,12 @@
 package org.mogoldb.weberpBackend.controller.v1.autenticacao.payload.request
 
+import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 
 open class CadastroRequest(
     @get: NotNull @get: NotBlank open var nome: String? = null,
-    @get: NotNull @get: NotBlank open var email: String? = null,
+    @get: NotNull @get: NotBlank @get: Email open var email: String? = null,
     @get: NotNull @get: NotBlank open var senha: String? = null,
     open var telefone: String? = null
 )
