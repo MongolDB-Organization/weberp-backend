cf59c6762482057d6403aee0fba4f0d291324422
Gabriel Pagotto <gabriel.pagotto@icloud.com>
2023-01-21 20:32:59 -0300

    improvements: create interface pattern to "LicencaService"

--- src/main/kotlin/org/mogoldb/weberpBackend/service/LicencaService.kt
+++ src/main/kotlin/org/mogoldb/weberpBackend/service/LicencaService.kt
@@ -1,10 +1,3 @@
 package org.mogoldb.weberpBackend.service
 
-import org.mogoldb.weberpBackend.delegate.service.NSService
-import org.mogoldb.weberpBackend.entity.Licenca
-import org.mogoldb.weberpBackend.repository.LicencaRepository
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Service
-
-@Service
-class LicencaService(@Autowired private val repository: LicencaRepository) : NSService<Licenca>(repository)
+interface LicencaService
